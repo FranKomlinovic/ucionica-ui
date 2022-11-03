2cf31fecdf7be98dbcdd7e708f2628f3689fe6b2
frankomlinovic <fran.komlinovic@gmail.com>
2022-11-03 16:25:45 +0100

    Fixed minor issues

--- src/app/components/user-create/user-create.component.ts
+++ src/app/components/user-create/user-create.component.ts
@@ -1,11 +1,13 @@
 import {Component, OnInit} from '@angular/core';
 import {Auth} from 'aws-amplify';
+import {MessageService} from "primeng/api";
 
 
 @Component({
   selector: 'app-user-create',
   templateUrl: './user-create.component.html',
-  styleUrls: ['./user-create.component.scss']
+  styleUrls: ['./user-create.component.scss'],
+  providers: [MessageService]
 })
 export class UserCreateComponent implements OnInit {
 
@@ -13,21 +15,50 @@ export class UserCreateComponent implements OnInit {
   givenName: string;
   familyName: string;
   password: string;
+  passwordValidation: string;
+  spinnerOn: boolean = false;
 
-  constructor() {
+  constructor(private messageService: MessageService) {
   }
 
   ngOnInit(): void {
   }
 
-  public signUp(): Promise<any> {
-    return Auth.signUp({
+  isValid(): boolean {
+    return !(this.password === this.passwordValidation && this.password != null && this.email != null && this.familyName != null && this.givenName != null && this.password.length > 5);
+  }
+
+  public signUp() {
+    this.spinnerOn = true;
+    Auth.signUp({
       username: this.email,
       password: this.password,
       attributes: {
         given_name: this.givenName,
         family_name: this.familyName,
       }
-    }).then(res => console.log(res));
+    }).then(res => {
+      if (res.userConfirmed) {
+        this.showSuccess(this.givenName + " " + this.familyName + " kreiran")
+      }
+    }).catch(reason => {
+      this.showError(reason.message);
+    }).finally(() => this.spinnerOn = false);
+  }
+
+  showSuccess(message: string) {
+    this.messageService.add({
+      severity: 'success',
+      summary: 'Success',
+      detail: message,
+    });
+  }
+
+  showError(message: string) {
+    this.messageService.add({
+      severity: 'error',
+      summary: 'Error',
+      detail: message,
+    });
   }
 }
