387ef020a8fcb63d714dadccb56ab6b688ce95aa
frankomlinovic <fran.komlinovic@gmail.com>
2022-11-28 15:57:18 +0100

    Reformatted current stays

--- src/app/components/current-stays/current-stays.component.ts
+++ src/app/components/current-stays/current-stays.component.ts
@@ -1,154 +1,154 @@
-import { Component, OnInit } from '@angular/core';
-import { ICurrentStay } from '../../interfaces/current-stay.interface';
-import {
-  ConfirmationService,
-  MessageService,
-  PrimeNGConfig,
-} from 'primeng/api';
-import { IUser } from '../../interfaces/user.interface';
-import { BackendService } from '../../backend.service';
-import { Auth } from 'aws-amplify';
+import { Component, OnInit } from "@angular/core";
+import { ICurrentStay } from "../../interfaces/current-stay.interface";
+import { ConfirmationService, MessageService } from "primeng/api";
+import { IUser } from "../../interfaces/user.interface";
+import { BackendService } from "../../backend.service";
+import { Auth } from "aws-amplify";
+import { Subject } from "rxjs";
+import { takeUntil } from "rxjs/operators";
+import { FeedbackService } from "../../services/feedback.service";
+import { HttpErrorResponse } from "@angular/common/http";
 
 @Component({
-  selector: 'app-current-stays',
-  templateUrl: './current-stays.component.html',
-  styleUrls: ['./current-stays.component.scss'],
-  providers: [MessageService],
+	selector: "app-current-stays",
+	templateUrl: "./current-stays.component.html",
+	styleUrls: ["./current-stays.component.scss"],
+	providers: [MessageService]
 })
 export class CurrentStaysComponent implements OnInit {
-  //Display controls
-  displayDialog: boolean = false;
-  spinnerOn: boolean = false;
-  admin: boolean = false;
-  //Form controls
-  selectedUserAdvanced: IUser[];
-  date: Date = new Date();
-  time: Date = new Date();
-  users: IUser[] = [];
-  //Grid controls
-  numberOfStays: number = 0;
-  currentStays: ICurrentStay[] = [];
-  //User details
-  selectedUserId: string;
-  showDialog: boolean;
+	users$ = new Subject<IUser[]>();
+	destroy$ = new Subject<boolean>();
+	allStays$ = new Subject<ICurrentStay[]>();
 
-  constructor(
-    private backendService: BackendService,
-    private confirmationService: ConfirmationService,
-    private messageService: MessageService,
-    private primengConfig: PrimeNGConfig
-  ) {}
+	//Display controls
+	displayDialog: boolean = false;
+	spinnerOn: boolean = false;
+	admin: boolean = false;
+	//Form controls
+	selectedUserAdvanced: IUser[];
+	date: Date = new Date();
+	//Grid controls
+	numberOfStays: number;
+	//User details
+	selectedUserId: string;
+	infoDialog: boolean;
 
-  ngOnInit(): void {
-    this.getAllStays();
-    this.primengConfig.ripple = true;
+	constructor(
+		private backendService: BackendService,
+		private confirmationService: ConfirmationService,
+		private messageService: MessageService,
+		private feedbackService: FeedbackService
+	) {}
 
-    Auth.currentAuthenticatedUser().then((data) => {
-      let groups = data.signInUserSession.accessToken.payload['cognito:groups'];
-      if (groups === undefined) {
-        this.admin = false;
-        return;
-      }
-      if (groups.includes('Generali')) {
-        this.admin = true;
-      }
-    });
-  }
+	ngOnInit(): void {
+		this.loadUsers();
+		this.loadCurrentStays();
+		this.checkIfAdmin();
+	}
 
-  getAllStays() {
-    this.spinnerOn = true;
-    this.backendService.getAllStays().subscribe((data: ICurrentStay[]) => {
-      this.currentStays = data;
-      this.numberOfStays = data.length;
-      this.displayDialog = false;
-      this.spinnerOn = false;
-    });
+	checkIfAdmin() {
+		Auth.currentAuthenticatedUser().then(data => {
+			let groups = data.signInUserSession.accessToken.payload["cognito:groups"];
+			if (groups === undefined) {
+				this.admin = false;
+				return;
+			}
+			if (groups.includes("Generali")) {
+				this.admin = true;
+			}
+		});
+	}
 
-    this.backendService.getUsers().subscribe((data) => {
-      this.users = data;
-      this.selectedUserAdvanced = [];
-    });
-  }
+	loadUsers() {
+		this.backendService
+			.getUsers()
+			.pipe(takeUntil(this.destroy$))
+			.subscribe((users: IUser[]) => {
+				this.users$.next(users);
+			});
+	}
 
-  evidentStay(userId: string, date: Date) {
-    this.spinnerOn = true;
-    this.backendService
-      .postStay(userId, date)
-      .pipe()
-      .subscribe((response) => {
-        this.showSuccess(response.message);
-        this.getAllStays();
-        this.spinnerOn = false;
-      });
-  }
+	loadCurrentStays(): void {
+		this.feedbackService.spinner$.next(true);
+		this.backendService
+			.getAllStays()
+			.pipe(takeUntil(this.destroy$))
+			.subscribe((events: ICurrentStay[]) => {
+				this.allStays$.next(events);
+				this.numberOfStays = events.length;
+				this.feedbackService.spinner(false);
+			});
+	}
 
-  endAllStays() {
-    this.spinnerOn = true;
-    this.backendService
-      .endAllStays()
-      .pipe()
-      .subscribe((response) => {
-        this.showSuccess(response.message);
-        this.getAllStays();
-        this.spinnerOn = false;
-      });
-  }
+	evidentStay(userId: string, date: Date) {
+		this.feedbackService.spinner$.next(true);
+		this.backendService.postStay(userId, date).subscribe({
+			next: a => {
+				this.feedbackService.successToast(a.message);
+				this.loadCurrentStays();
+				this.displayDialog = false;
+			},
+			error: (err: HttpErrorResponse) => {
+				this.displayDialog = false;
+				this.feedbackService.errorToast(err.message);
+			}
+		});
+	}
 
-  openDialog() {
-    this.displayDialog = true;
-    this.date = new Date();
-    this.time = new Date();
-  }
+	endAllStays() {
+		this.feedbackService.spinner$.next(true);
+		this.backendService.endAllStays().subscribe(response => {
+			this.feedbackService.successToast(response.message);
+			this.loadCurrentStays();
+		});
+	}
 
-  saveStay() {
-    this.spinnerOn = true;
-    if (this.selectedUserAdvanced === undefined) {
-      return;
-    }
-    this.date.setTime(this.time.getTime());
-    for (const userModel of this.selectedUserAdvanced) {
-      this.evidentStay(userModel.id, this.date);
-    }
-  }
+	openDialog() {
+		this.displayDialog = true;
+		this.date = new Date();
+	}
 
-  confirmEndStay(id: string) {
-    this.confirmationService.confirm({
-      message: 'Jeste li sigurni da želite završiti dejstvo za ovog korisnika?',
-      icon: 'pi pi-exclamation-triangle',
-      accept: () => {
-        this.evidentStay(id, new Date());
-        this.confirmationService.close();
-      },
-      reject: () => {
-        this.confirmationService.close();
-      },
-    });
-  }
+	saveStay() {
+		this.feedbackService.spinner$.next(true);
+		if (this.selectedUserAdvanced === undefined) {
+			return;
+		}
+		for (const userModel of this.selectedUserAdvanced) {
+			this.evidentStay(userModel.id, this.date);
+		}
+	}
 
-  confirmEndAllStays() {
-    this.confirmationService.confirm({
-      message: 'Jeste li sigurni da želite završiti dejstvo za sve korisnike?',
-      icon: 'pi pi-exclamation-triangle',
-      accept: () => {
-        this.endAllStays();
-        this.confirmationService.close();
-      },
-      reject: () => {
-        this.confirmationService.close();
-      },
-    });
-  }
+	confirmEndStay(id: string) {
+		this.confirmationService.confirm({
+			message: "Jeste li sigurni da želite završiti dejstvo za ovog korisnika?",
+			icon: "pi pi-exclamation-triangle",
+			accept: () => {
+				this.evidentStay(id, new Date());
+				this.confirmationService.close();
+				this.loadCurrentStays();
+			},
+			reject: () => {
+				this.confirmationService.close();
+			}
+		});
+	}
 
-  showSuccess(message: string) {
-    this.messageService.add({
-      severity: 'success',
-      summary: 'Success',
-      detail: message,
-    });
-  }
+	confirmEndAllStays() {
+		this.confirmationService.confirm({
+			message: "Jeste li sigurni da želite završiti dejstvo za sve korisnike?",
+			icon: "pi pi-exclamation-triangle",
+			accept: () => {
+				this.endAllStays();
+				this.confirmationService.close();
+			},
+			reject: () => {
+				this.confirmationService.close();
+			}
+		});
+	}
 
-  showInfo(userId: string) {
-    this.selectedUserId = userId;
-    this.showDialog = true;
-  }
+	showInfo(userId: string) {
+		this.selectedUserId = userId;
+		this.infoDialog = true;
+	}
 }
