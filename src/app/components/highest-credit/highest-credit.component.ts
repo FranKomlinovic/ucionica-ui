d71b550a61f83347ebd591ebf9ccd9859de7f9bf
frankomlinovic <fran.komlinovic@gmail.com>
2022-11-28 19:09:03 +0100

    Small bugfixes, user details update

--- src/app/components/highest-credit/highest-credit.component.ts
+++ src/app/components/highest-credit/highest-credit.component.ts
@@ -1,27 +1,45 @@
-import { Component, OnInit } from '@angular/core';
-import { IHighestDebt } from '../../interfaces/highest-debt.interface';
-import { BackendService } from '../../backend.service';
+import { Component, OnInit } from "@angular/core";
+import { IHighestDebt } from "../../interfaces/highest-debt.interface";
+import { BackendService } from "../../backend.service";
+import { takeUntil } from "rxjs/operators";
+import { FeedbackService } from "../../services/feedback.service";
+import { Subject } from "rxjs";
 
 @Component({
-  selector: 'app-highest-credit',
-  templateUrl: './highest-credit.component.html',
-  styleUrls: ['./highest-credit.component.scss'],
+	selector: "app-highest-credit",
+	templateUrl: "./highest-credit.component.html",
+	styleUrls: ["./highest-credit.component.scss"]
 })
 export class HighestCreditComponent implements OnInit {
-  highestCredit: IHighestDebt[] = [];
-  spinnerOn: boolean = false;
+	destroy$ = new Subject<boolean>();
+	highestCredit$ = new Subject<IHighestDebt[]>();
 
-  constructor(private backendService: BackendService) {}
+	//User details
+	selectedUserId: string;
+	infoDialog: boolean;
 
-  ngOnInit(): void {
-    this.getHighestDebt();
-  }
+	constructor(
+		private backendService: BackendService,
+		private feedbackService: FeedbackService
+	) {}
 
-  getHighestDebt() {
-    this.spinnerOn = true;
-    this.backendService.getHighestCredit().subscribe((data) => {
-      this.highestCredit = data;
-      this.spinnerOn = false;
-    });
-  }
+	ngOnInit(): void {
+		this.getHighestCredit();
+	}
+
+	getHighestCredit(): void {
+		this.feedbackService.spinner$.next(true);
+		this.backendService
+			.getHighestCredit()
+			.pipe(takeUntil(this.destroy$))
+			.subscribe((events: IHighestDebt[]) => {
+				this.highestCredit$.next(events);
+				this.feedbackService.spinner(false);
+			});
+	}
+
+	showInfo(userId: string) {
+		this.selectedUserId = userId;
+		this.infoDialog = true;
+	}
 }
--- src/app/interfaces/user-details.interface.ts
+++ src/app/interfaces/user-details.interface.ts
@@ -1,12 +1,17 @@
-import { IUserPayment } from './user-payment.interface';
-import { IUserStay } from './user-stay.interface';
+import { IUserPayment } from "./user-payment.interface";
+import { IUserStay } from "./user-stay.interface";
 
 export class IUserDetails {
-  id: string;
-  username: string;
-  balance: string;
-  picture: string;
-  currentlyActive: boolean;
-  stays: IUserStay[];
-  payments: IUserPayment[];
+	id: string;
+	username: string;
+	balance: string;
+	picture: string;
+	timeSpent: string;
+	averageTimeSpent: string;
+	totalPayments: string;
+	totalStayPrice: string;
+	averageStayPrice: string;
+	currentlyActive: boolean;
+	stays: IUserStay[];
+	payments: IUserPayment[];
 }
