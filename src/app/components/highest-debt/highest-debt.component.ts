0f97500b4fb7b96f93065c3d2864be167ae431a7
frankomlinovic <fran.komlinovic@gmail.com>
2022-11-29 14:04:13 +0100

    Add highest debt new impl

--- src/app/components/highest-debt/highest-debt.component.ts
+++ src/app/components/highest-debt/highest-debt.component.ts
@@ -1,32 +1,45 @@
-import { Component, OnInit } from '@angular/core';
-import { IHighestDebt } from '../../interfaces/highest-debt.interface';
-import { BackendService } from '../../backend.service';
-import { FeedbackService } from 'src/app/services/feedback.service';
+import { Component, OnInit } from "@angular/core";
+import { IHighestDebt } from "../../interfaces/highest-debt.interface";
+import { BackendService } from "../../backend.service";
+import { takeUntil } from "rxjs/operators";
+import { FeedbackService } from "../../services/feedback.service";
+import { Subject } from "rxjs";
 
 @Component({
-  selector: 'app-highest-debt',
-  templateUrl: './highest-debt.component.html',
-  styleUrls: ['./highest-debt.component.scss'],
+	selector: "app-highest-debt",
+	templateUrl: "./highest-debt.component.html",
+	styleUrls: ["./highest-debt.component.scss"]
 })
 export class HighestDebtComponent implements OnInit {
-  highestDebt: IHighestDebt[] = [];
+	destroy$ = new Subject<boolean>();
+	highestDebt$ = new Subject<IHighestDebt[]>();
 
-  constructor(
-    private backendService: BackendService,
-    private feedbackService: FeedbackService
-  ) {}
+	//User details
+	selectedUserId: string;
+	infoDialog: boolean;
 
-  ngOnInit(): void {
-    this.getHighestDebt();
-  }
+	constructor(
+		private backendService: BackendService,
+		private feedbackService: FeedbackService
+	) {}
 
-  getHighestDebt() {
-    this.backendService
-      .getHighestDebt()
-      //   .pipe(tap(() => this.feedbackService.spinner(false)))
-      .subscribe((data) => {
-        this.highestDebt = data;
-        // this.feedbackService.spinner(false);
-      });
-  }
+	ngOnInit(): void {
+		this.getHighestDebt();
+	}
+
+	getHighestDebt(): void {
+		this.feedbackService.spinner$.next(true);
+		this.backendService
+			.getHighestDebt()
+			.pipe(takeUntil(this.destroy$))
+			.subscribe((events: IHighestDebt[]) => {
+				this.highestDebt$.next(events);
+				this.feedbackService.spinner(false);
+			});
+	}
+
+	showInfo(userId: string) {
+		this.selectedUserId = userId;
+		this.infoDialog = true;
+	}
 }
