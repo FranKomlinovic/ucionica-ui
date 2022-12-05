ec3739009a555c86191ebab0fd2b27840b32437a
Teo Kolić <kolic822@gmail.com>
2022-12-05 12:35:47 +0100

    Card refactor

--- src/app/components/last-payments/last-payments.component.ts
+++ src/app/components/last-payments/last-payments.component.ts
@@ -1,163 +1,165 @@
-import { Component, OnInit } from "@angular/core";
-import { IPayment } from "../../interfaces/payments.interface";
-import { ConfirmationService, MessageService, SelectItem } from "primeng/api";
-import { IUser } from "../../interfaces/user.interface";
-import { BackendService } from "../../backend.service";
-import { BehaviorSubject, Subject } from "rxjs";
+import { Component, OnInit } from '@angular/core';
+import { IPayment } from '../../interfaces/payments.interface';
+import { ConfirmationService, MessageService, SelectItem } from 'primeng/api';
+import { IUser } from '../../interfaces/user.interface';
+import { BackendService } from '../../backend.service';
+import { BehaviorSubject, Subject } from 'rxjs';
 import {
-	AbstractControl,
-	FormControl,
-	FormGroup,
-	Validators
-} from "@angular/forms";
-import { map, switchMap, takeUntil, tap } from "rxjs/operators";
-import { FeedbackService } from "../../services/feedback.service";
-import { HttpErrorResponse } from "@angular/common/http";
-import { CreatePaymentModel } from "src/app/models/create-payment.model";
-import { IUserDetails } from "src/app/interfaces/user-details.interface";
+    AbstractControl,
+    FormControl,
+    FormGroup,
+    Validators,
+} from '@angular/forms';
+import { map, switchMap, takeUntil, tap } from 'rxjs/operators';
+import { FeedbackService } from '../../services/feedback.service';
+import { HttpErrorResponse } from '@angular/common/http';
+import { CreatePaymentModel } from 'src/app/models/create-payment.model';
+import { IUserDetails } from 'src/app/interfaces/user-details.interface';
 
 @Component({
-	selector: "app-last-payments",
-	templateUrl: "./last-payments.component.html",
-	styleUrls: ["./last-payments.component.scss"],
-	providers: [MessageService]
+    selector: 'app-last-payments',
+    templateUrl: './last-payments.component.html',
+    styleUrls: ['./last-payments.component.scss'],
+    providers: [MessageService],
 })
 export class LastPaymentsComponent implements OnInit {
-	users: IUser[] = [];
+    users: IUser[] = [];
 
-	allPayments$ = new Subject<IPayment[] | null>();
-	users$ = new BehaviorSubject<IUser[]>(this.users);
-	destroy$ = new Subject<boolean>();
+    allPayments$ = new BehaviorSubject<IPayment[] | null>(null);
+    users$ = new BehaviorSubject<IUser[]>(this.users);
+    destroy$ = new Subject<boolean>();
 
-	formGroup = new FormGroup({
-		id: new FormControl<string>(""),
-		userId: new FormControl<string>("", {
-			nonNullable: true,
-			validators: Validators.required
-		}),
-		time: new FormControl<Date | null>(new Date(), {
-			validators: Validators.required
-		}),
-		description: new FormControl<string>(""),
-		amount: new FormControl<number>(100, {
-			nonNullable: true,
-			validators: Validators.required
-		})
-	});
+    formGroup = new FormGroup({
+        id: new FormControl<string>(''),
+        userId: new FormControl<string>('', {
+            nonNullable: true,
+            validators: Validators.required,
+        }),
+        time: new FormControl<Date | null>(new Date(), {
+            validators: Validators.required,
+        }),
+        description: new FormControl<string>(''),
+        amount: new FormControl<number>(100, {
+            nonNullable: true,
+            validators: Validators.required,
+        }),
+    });
 
-	//Display controls
-	displayDialog: boolean = false;
+    //Display controls
+    displayDialog: boolean = false;
 
-	constructor(
-		private backendService: BackendService,
-		private confirmationService: ConfirmationService,
-		private feedbackService: FeedbackService
-	) {}
+    constructor(
+        private backendService: BackendService,
+        private confirmationService: ConfirmationService,
+        private feedbackService: FeedbackService
+    ) {}
 
-	ngOnInit(): void {
-		this.loadPayments();
-		this.loadUsers();
-	}
+    ngOnInit(): void {
+        this.loadPayments();
+        this.loadUsers();
+    }
 
-	filterUsers(event: { ogEvent: PointerEvent; query: string }) {
-		this.users = this.users$.value.filter((user: IUser) =>
-			user.username.toLowerCase().includes(event.query.toLocaleLowerCase())
-		);
-	}
+    filterUsers(event: { ogEvent: PointerEvent; query: string }) {
+        this.users = this.users$.value.filter((user: IUser) =>
+            user.username
+                .toLowerCase()
+                .includes(event.query.toLocaleLowerCase())
+        );
+    }
 
-	selectUser(user: IUser) {
-		this.formGroup
-			.get("userId")
-			?.setValue(user.id, { emitModelToViewChange: false });
-	}
+    selectUser(user: IUser) {
+        this.formGroup
+            .get('userId')
+            ?.setValue(user.id, { emitModelToViewChange: false });
+    }
 
-	loadPayments(): void {
-		this.allPayments$.next(null);
-		this.backendService
-			.getAllPayments()
-			.pipe(takeUntil(this.destroy$))
-			.subscribe((payments: IPayment[]) => {
-				this.allPayments$.next(payments);
-			});
-	}
+    loadPayments(): void {
+        this.allPayments$.next(null);
+        this.backendService
+            .getAllPayments()
+            .pipe(takeUntil(this.destroy$))
+            .subscribe((payments: IPayment[]) => {
+                this.allPayments$.next(payments);
+            });
+    }
 
-	loadUsers() {
-		this.backendService
-			.getUsers()
-			.pipe(takeUntil(this.destroy$))
-			.subscribe((users: IUser[]) => {
-				this.users = users;
-				this.users$.next(users);
-			});
-	}
+    loadUsers() {
+        this.backendService
+            .getUsers()
+            .pipe(takeUntil(this.destroy$))
+            .subscribe((users: IUser[]) => {
+                this.users = users;
+                this.users$.next(users);
+            });
+    }
 
-	deleteClick(id: string) {
-		this.confirmationService.confirm({
-			message: "Jeste li sigurni da želite obrisati uplatu?",
-			accept: () => {
-				this.deletePayment(id);
-			}
-		});
-	}
+    deleteClick(id: string) {
+        this.confirmationService.confirm({
+            message: 'Jeste li sigurni da želite obrisati uplatu?',
+            accept: () => {
+                this.deletePayment(id);
+            },
+        });
+    }
 
-	deletePayment(id: string) {
-		this.allPayments$.next(null);
-		this.backendService.deletePayment(id).subscribe({
-			next: a => {
-				this.loadPayments();
-				this.feedbackService.successToast(a.message);
-			},
-			error: e => {
-				this.feedbackService.errorToast(e.message);
-			}
-		});
-	}
+    deletePayment(id: string) {
+        this.allPayments$.next(null);
+        this.backendService.deletePayment(id).subscribe({
+            next: (a) => {
+                this.loadPayments();
+                this.feedbackService.successToast(a.message);
+            },
+            error: (e) => {
+                this.feedbackService.errorToast(e.message);
+            },
+        });
+    }
 
-	createPayment() {
-		const paymentData = this.formGroup.getRawValue();
+    createPayment() {
+        const paymentData = this.formGroup.getRawValue();
 
-		this.feedbackService.spinner(true);
-		this.backendService.createPayment(paymentData).subscribe({
-			next: a => {
-				this.feedbackService.successToast(a.message);
-				this.loadPayments();
-				this.displayDialog = false;
-			},
-			error: (err: HttpErrorResponse) => {
-				this.displayDialog = false;
-				this.feedbackService.errorToast(err.message);
-			}
-		});
-	}
+        this.feedbackService.spinner(true);
+        this.backendService.createPayment(paymentData).subscribe({
+            next: (a) => {
+                this.feedbackService.successToast(a.message);
+                this.loadPayments();
+                this.displayDialog = false;
+            },
+            error: (err: HttpErrorResponse) => {
+                this.displayDialog = false;
+                this.feedbackService.errorToast(err.message);
+            },
+        });
+    }
 
-	editPayment(paymentId: string) {
-		this.openDialog();
-		this.backendService
-			.getPaymentById(paymentId)
-			.pipe(
-				tap(() => this.feedbackService.spinner(true)),
-				switchMap(payment =>
-					this.backendService.getUserDetails(payment.userId).pipe(
-						map((user: IUserDetails) => ({
-							...payment,
-							userId: user,
-							time: new Date(payment.time!)
-						}))
-					)
-				)
-			)
-			.subscribe(payment => {
-				this.formGroup.patchValue(payment as unknown as IUserDetails);
-				this.feedbackService.spinner(false);
-			});
-	}
+    editPayment(paymentId: string) {
+        this.openDialog();
+        this.backendService
+            .getPaymentById(paymentId)
+            .pipe(
+                tap(() => this.feedbackService.spinner(true)),
+                switchMap((payment) =>
+                    this.backendService.getUserDetails(payment.userId).pipe(
+                        map((user: IUserDetails) => ({
+                            ...payment,
+                            userId: user,
+                            time: new Date(payment.time!),
+                        }))
+                    )
+                )
+            )
+            .subscribe((payment) => {
+                this.formGroup.patchValue(payment as unknown as IUserDetails);
+                this.feedbackService.spinner(false);
+            });
+    }
 
-	openDialog() {
-		this.displayDialog = true;
-	}
+    openDialog() {
+        this.displayDialog = true;
+    }
 
-	ngOnDestroy() {
-		this.destroy$.next(true);
-		this.destroy$.complete();
-	}
+    ngOnDestroy() {
+        this.destroy$.next(true);
+        this.destroy$.complete();
+    }
 }
--- src/app/components/user-info/user-info.component.ts
+++ src/app/components/user-info/user-info.component.ts
@@ -1,105 +1,107 @@
-import { Component, Input, OnInit, ViewChild } from "@angular/core";
-import { BackendService } from "../../backend.service";
-import { ConfirmationService, MessageService } from "primeng/api";
-import { IUserDetails } from "../../interfaces/user-details.interface";
-import { IEvent } from "../../interfaces/event.interface";
-import { delay, takeUntil } from "rxjs/operators";
-import { FeedbackService } from "../../services/feedback.service";
-import { BehaviorSubject, Subject } from "rxjs";
-import { Auth, Storage } from "aws-amplify";
-import { FileUpload } from "primeng/fileupload";
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
+import { BackendService } from '../../backend.service';
+import { ConfirmationService, MessageService } from 'primeng/api';
+import { IUserDetails } from '../../interfaces/user-details.interface';
+import { IEvent } from '../../interfaces/event.interface';
+import { delay, takeUntil } from 'rxjs/operators';
+import { FeedbackService } from '../../services/feedback.service';
+import { BehaviorSubject, Subject } from 'rxjs';
+import { Auth, Storage } from 'aws-amplify';
+import { FileUpload } from 'primeng/fileupload';
 
 @Component({
-	selector: "app-user-info",
-	templateUrl: "./user-info.component.html",
-	styleUrls: ["./user-info.component.scss"],
-	providers: [MessageService]
+    selector: 'app-user-info',
+    templateUrl: './user-info.component.html',
+    styleUrls: ['./user-info.component.scss'],
+    providers: [MessageService],
 })
 export class UserInfoComponent implements OnInit {
-	@Input() userId: string;
-	@Input() readOnly: boolean;
-	@ViewChild(FileUpload) fileUpload: FileUpload;
+    @Input() userId: string;
+    @Input() readOnly: boolean;
+    @ViewChild(FileUpload) fileUpload: FileUpload;
 
-	userEvents$ = new Subject<IEvent[]>();
-	userDetails$ = new BehaviorSubject<IUserDetails>({} as IUserDetails);
-	destroy$ = new Subject<boolean>();
+    userEvents$ = new Subject<IEvent[]>();
+    userDetails$ = new BehaviorSubject<IUserDetails | null>(null);
+    destroy$ = new Subject<boolean>();
 
-	constructor(
-		private backendService: BackendService,
-		private confirmationService: ConfirmationService,
-		private feedbackService: FeedbackService
-	) {}
+    constructor(
+        private backendService: BackendService,
+        private confirmationService: ConfirmationService,
+        private feedbackService: FeedbackService
+    ) {}
 
-	ngOnInit() {
-		this.loadUserDetails();
-		this.loadUserEvents();
-	}
+    ngOnInit() {
+        this.loadUserDetails();
+        this.loadUserEvents();
+    }
 
-	loadUserEvents(): void {
-		this.backendService
-			.getEventsByUserId(this.userId)
-			.pipe(takeUntil(this.destroy$))
-			.subscribe((events: IEvent[]) => {
-				this.userEvents$.next(events);
-			});
-	}
+    loadUserEvents(): void {
+        this.backendService
+            .getEventsByUserId(this.userId)
+            .pipe(takeUntil(this.destroy$))
+            .subscribe((events: IEvent[]) => {
+                this.userEvents$.next(events);
+            });
+    }
 
-	loadUserDetails(): void {
-		this.backendService
-			.getUserDetails(this.userId)
-			.pipe(takeUntil(this.destroy$))
-			.subscribe((details: IUserDetails) => {
-				this.userDetails$.next(details);
-			});
-	}
+    loadUserDetails(): void {
+        this.backendService
+            .getUserDetails(this.userId)
+            .pipe(takeUntil(this.destroy$))
+            .subscribe((details: IUserDetails) => {
+                this.userDetails$.next(details);
+            });
+    }
 
-	evidentStay(userId: string) {
-		this.feedbackService.spinner$.next(true);
-		this.backendService.postStay(userId, new Date()).subscribe(response => {
-			this.feedbackService.successToast(response.message);
-			this.loadUserDetails();
-			this.confirmationService.close();
-		});
-	}
+    evidentStay(userId: string) {
+        this.feedbackService.spinner$.next(true);
+        this.backendService
+            .postStay(userId, new Date())
+            .subscribe((response) => {
+                this.feedbackService.successToast(response.message);
+                this.loadUserDetails();
+                this.confirmationService.close();
+            });
+    }
 
-	confirmStay() {
-		const isUserActive = this.userDetails$.value.currentlyActive;
+    confirmStay() {
+        const isUserActive = this.userDetails$.value?.currentlyActive;
 
-		this.confirmationService.confirm({
-			message: isUserActive
-				? "Želite li se odjaviti iz učionice?"
-				: "Želite li se prijaviti u učionicu?",
-			icon: "pi pi-exclamation-triangle",
-			accept: () => {
-				this.evidentStay(this.userDetails$.value.id);
-			},
-			reject: () => {
-				this.confirmationService.close();
-			}
-		});
-	}
+        this.confirmationService.confirm({
+            message: isUserActive
+                ? 'Želite li se odjaviti iz učionice?'
+                : 'Želite li se prijaviti u učionicu?',
+            icon: 'pi pi-exclamation-triangle',
+            accept: () => {
+                this.evidentStay(this.userDetails$.value?.id!);
+            },
+            reject: () => {
+                this.confirmationService.close();
+            },
+        });
+    }
 
-	upload(event: any) {
-		this.feedbackService.spinner$.next(true);
+    upload(event: any) {
+        this.feedbackService.spinner$.next(true);
 
-		Auth.currentAuthenticatedUser().then(a => {
-			let file = event.files[0];
-			Storage.put(file.name, file).then(result => {
-				Auth.updateUserAttributes(a, {
-					picture:
-						"https://ucionica-pictures.s3.eu-central-1.amazonaws.com/public/" +
-						result.key
-				}).then(() => {
-					this.feedbackService.spinner(false);
-					this.fileUpload.clear();
-					this.loadUserDetails();
-				});
-			});
-		});
-	}
+        Auth.currentAuthenticatedUser().then((a) => {
+            let file = event.files[0];
+            Storage.put(file.name, file).then((result) => {
+                Auth.updateUserAttributes(a, {
+                    picture:
+                        'https://ucionica-pictures.s3.eu-central-1.amazonaws.com/public/' +
+                        result.key,
+                }).then(() => {
+                    this.feedbackService.spinner(false);
+                    this.fileUpload.clear();
+                    this.loadUserDetails();
+                });
+            });
+        });
+    }
 
-	ngOnDestroy() {
-		this.destroy$.next(true);
-		this.destroy$.complete();
-	}
+    ngOnDestroy() {
+        this.destroy$.next(true);
+        this.destroy$.complete();
+    }
 }
